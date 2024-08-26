0a982d3ecd2ea77994e666a36d50fa05fa571a5a
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2024-08-26 18:42:12 +0530

    initialize as a firebase project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/Homepage";
 
 function App() {
   return (
     <div>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
